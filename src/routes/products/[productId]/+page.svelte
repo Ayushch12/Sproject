afa8d017e2988cc11070bcad93917e016c8a5a21
Ayush chalisee <ayushchalise12@gmail.com>
2023-10-28 22:59:44 +0200

    Dynamic Routes

---
+++ src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,8 @@
+
+<script>
+    import { page } from "$app/stores";
+    const productId = $page.params.productId  // Params : it use to pass date .
+</script>
+
+
+<h1>Details about product {productId}</h1>
