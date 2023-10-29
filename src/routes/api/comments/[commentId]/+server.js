cc83b7fc48cedd9314cb4c1afadd0127047cf3bc
Ayush chalisee <ayushchalise12@gmail.com>
2023-10-29 20:18:08 +0100

    Handling Pathch request

--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -1,6 +1,9 @@
 import { comments } from "$lib/comments.js";
 import { json } from "@sveltejs/kit";
 
+
+//Dynamic API Routes :
+
 export function GET(requestEvent) {
     const { params } = requestEvent;
     const { commentId } = params;
@@ -8,4 +11,22 @@ export function GET(requestEvent) {
         (comment) => comment.id === parseInt(commentId)
     );
     return json(comment);
+}
+
+
+// Handling Path Request:
+
+export async function PATCH(requestEvent) {
+    const { params, request } = requestEvent;
+    const { commentId } = params;
+    const { text } = await request.json();
+
+    const comment = comments.find((comment) => comment.id === parseInt(commentId));
+
+    if (comment) {
+        comment.text = text;
+        return json(comment);
+    } else {
+        return json({ error: "Comment not found" }, { status: 404 });
+    }
 }
