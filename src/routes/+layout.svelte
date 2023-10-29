df72c12e7d85fabf3d227624d0d08055413d37de
Ayush chalisee <ayushchalise12@gmail.com>
2023-10-29 16:54:36 +0100

    Route Layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<header class="layout-header">Header</header>
+
+<!-- In Svelte, the <slot> element is used to create a slot that can be filled with content when
+    the component is used. It allows you to pass content from the
+     parent component to the child component. -->
+<slot />
+
+<footer class="layout-footer">Footer</footer>
+
+<style>
+    .layout-header{
+        background-color: #264653;
+        color: #f4a261;
+        font-size: 30px;
+        text-align: center;
+        padding: 1opx;
+    }
+
+    .layout-footer{
+        background-color: #264653;
+        color: #e9c46a;
+        font-size: 30px;
+        text-align: center;
+        padding: 10px;
+
+    }
+
+
+</style>
+
+
+
