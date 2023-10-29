3d16e7eb6e4c0303ad3dd157ae3dbf792b83354b
Ayush chalisee <ayushchalise12@gmail.com>
2023-10-29 19:00:53 +0100

    API get Request

---
+++ src/lib/comments.js
@@ -0,0 +1,18 @@
+export const comments = [
+
+    {
+
+        id:1,
+        text:"This is the first commemt"
+    },
+    {
+        id:2,
+        text:"This is the second comment"
+    },
+    {
+        id:3,
+        text:"This is the third comment"
+    }
+
+
+];
