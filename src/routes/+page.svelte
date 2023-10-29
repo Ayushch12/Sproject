f6504bb19b40f89be4a208d9738a8c3b2a37414a
Ayush chalisee <ayushchalise12@gmail.com>
2023-10-29 16:27:22 +0100

    Navigation Programmatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,29 @@
+<script>
+
+    import { afterNavigate, beforeNavigate, goto } from "$app/navigation";
+
+
+
+    const handleClick = () =>{
+        console.log("Placing your order");
+        goto("/products", {replaceState:true});
+    };
+
+
+    beforeNavigate((navigation)=>{
+        console.log({before: navigation})
+    });
+    afterNavigate((navigation)=>{
+        console.log({after: navigation})
+    });
+</script>
+
+
 <h1>Hey my world</h1>
+
+
 
 <a href="/blog">Blog</a>
 <a href="/products">Product</a>
+
+<button on:click={handleClick}>Place order</button>
